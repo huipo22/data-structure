b45b2a0ab7de6d54aff670a5b6cffa77ce4bcd0c
huipo22@163.com <P13834728300d>
2017-12-05 08:32:14 +0800

    常见算法

--- LinkedList.js
+++ LinkedList.js
@@ -116,3 +116,50 @@ list.append(15);
 list.append(10);
 list.insert(1,90);
 console.log(list.toString());
+
+//双向链表
+function DoublyLinkedList(){
+    var Node=function(element){
+        this.element=element;
+        this.next=null;
+        this.prev=null;
+    }
+    var length=0;
+    var head=null;
+    var tail=null;
+    this.insert=function(position,element){
+        if(position>=0 && position<=length){
+            var node=new Node(element),
+                current=head,
+                previous,
+                index=0;
+            if(position===0){
+                if(!head){
+                    head=node;
+                    tail=node;
+                }else{
+                    node.next=current;
+                    current.prev=node;
+                }
+            }else if(position===length){
+                current=tail;
+                current.next=node;
+                node.prev=current;
+                tail=node;
+            }else{
+                while(index++<position){
+                    previous=current;
+                    current=current.next;
+                }
+                node.next=current;
+                previous.next=node;
+                current.prev=node;
+                node.prev=previous;
+            }
+            length++;
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
